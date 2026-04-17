debdaf3514495abf45e17770396905eebca443e3
Ankush <deshwalankush23@gmail.com>
2026-04-17 09:26:00 +0530

    vercel config file added

--- mini_project/backend/config/db.js
+++ mini_project/backend/config/db.js
@@ -5,8 +5,9 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/food_delivery');
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
+        console.log(process.env.MONGO_URI)
         console.error('❌ MongoDB connection error:', err.message);
-        process.exit(1);
+        // process.exit(1);
     }
 };
 
